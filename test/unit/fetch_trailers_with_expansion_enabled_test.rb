f8747ec1fc3765ceac9468bf1b9fad32cf05194e
Loïc Guitaut <loic@studiomelipone.eu>
2012-11-19 10:22:21 +0100

    Fix typos.

--- test/unit/fetch_trailers_with_expansion_enabled_test.rb
+++ test/unit/fetch_trailers_with_expansion_enabled_test.rb
@@ -1,12 +1,11 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '..', 'test_helper.rb'))
 
-class TestFetchTrailersWithExpansionEnabled < Test::Unit::TestCase
-
+class FetchTrailersWithExpansionEnabled < Test::Unit::TestCase
   def setup
     register_api_url_stubs
   end
   
-  test "find with expanison enabled should return trailers" do
+  test "find with expansion enabled should return trailers" do
     movie = TmdbMovie.find(:id => 187)
     assert_not_nil movie.trailers
     assert_equal movie.trailers[0].source, 'SUXWAEX2jlg'
