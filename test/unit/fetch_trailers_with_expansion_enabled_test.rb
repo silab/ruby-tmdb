029d2341c426b8396d1a30dc6607a957a2960966
Jon Maddox <jon@jonmaddox.com>
2012-12-19 23:15:39 -0500

    back compat!

--- test/unit/fetch_trailers_with_expansion_enabled_test.rb
+++ test/unit/fetch_trailers_with_expansion_enabled_test.rb
@@ -4,7 +4,7 @@ class FetchTrailersWithExpansionEnabled < Test::Unit::TestCase
   def setup
     register_api_url_stubs
   end
-  
+
   test "find with expansion enabled should return trailers" do
     movie = TmdbMovie.find(:id => 187)
     assert_not_nil movie.trailers
@@ -12,7 +12,7 @@ class FetchTrailersWithExpansionEnabled < Test::Unit::TestCase
   end
 
   test "find without expansion enabled should not return trailers" do
-    movie = TmdbMovie.find :id => 187, expand_results: false
+    movie = TmdbMovie.find :id => 187, :expand_results => false
     assert_nil movie.trailers
   end
 end
